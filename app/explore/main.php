b6a15ccefec2c62547d934b0eea322ebb6090f01
493405455 <zhuyunxiang@users.noreply.github.com>
2016-03-12 11:45:28 +0000

    1. 修改发现为热门内容

--- app/explore/main.php
+++ app/explore/main.php
@@ -54,7 +54,7 @@ class main extends AWS_CONTROLLER
 
 		if ($this->user_id)
 		{
-			$this->crumb(AWS_APP::lang()->_t('热门内容'), '/explore');
+			$this->crumb(AWS_APP::lang()->_t('发现'), '/explore');
 
 			if (! $this->user_info['email'])
 			{
